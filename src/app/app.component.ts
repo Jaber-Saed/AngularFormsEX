ef9722ea09b77a5360d62adcf0d6afa22cd1ba29
Jaber-Saed <92320333+Jaber-Saed@users.noreply.github.com>
2023-09-05 14:23:43 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'AngularForms';
+  onSubmit(form: NgForm) {
+    console.warn(form);
+  }
 }
