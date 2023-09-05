ecbc995b9ff87ba8462c36bfb2594887e60e3d9b
Jaber-Saed <92320333+Jaber-Saed@users.noreply.github.com>
2023-09-05 14:56:25 +0300

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -8,7 +8,8 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   title = 'AngularForms';
-  onSubmit(form: NgForm) {
-    console.warn(form);
+  @ViewChild('myForm') form?: NgForm;
+  onSubmit() {
+    console.warn(this.form);
   }
 }
